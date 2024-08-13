ff41060a07e5de992e68ed98daf379587e8359e4
Varun Nagda <vnagda1549@conestogac.on.ca>
2024-08-13 10:57:22 -0400

    build and changed live url

--- client/src/apolloCLient.js
+++ client/src/apolloCLient.js
@@ -2,9 +2,9 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 // Determine the GraphQL server URI based on the current URL
 const uri =
-  window.location.hostname === "localhost"
+  window.location.hostname == "localhost"
     ? "http://localhost:4000/graphql" // Local development
-    : "https://your-server-url.onrender.com/graphql"; // Production (Render)
+    : "https://manishmasala.onrender.com/graphql"; // Production (Render)
 
 const client = new ApolloClient({
   uri: uri,
